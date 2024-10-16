dfe15d6159a422e598af3c8d03059dfaa63b430f
Andrei-Constantin <andrei-constantin.popa@estudiantat.upc.edu>
2024-10-16 13:23:45 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import {
+  Navbar,
+  Hero,
+  About,
+  Experience,
+  Tech,
+  Works,
+  Feedbacks,
+} from "./components/components";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
