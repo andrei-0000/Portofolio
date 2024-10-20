1085b4c714ecd667a56e9862ee2d06d3a21fcc45
Andrei-Constantin <andrei-constantin.popa@estudiantat.upc.edu>
2024-10-20 16:03:41 +0200

    sectionwrapper

--- src/components/components/About.jsx
+++ src/components/components/About.jsx
@@ -5,6 +5,7 @@ import { styles } from "../../styles";
 import { services } from "../../constants";
 import { fadeIn, textVariant } from "../../utils/motion";
 import { style } from "framer-motion/client";
+import { SectionWrapper } from "../../hoc";
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
@@ -62,4 +63,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default SectionWrapper(About, "about");
