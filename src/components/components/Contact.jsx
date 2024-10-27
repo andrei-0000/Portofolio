0e26329f39e0622bb848c12dc7d95bcbf8e4a914
Andrei-Constantin <andrei-constantin.popa@estudiantat.upc.edu>
2024-10-27 14:46:51 +0100

    added earth

--- src/components/components/Contact.jsx
+++ src/components/components/Contact.jsx
@@ -22,7 +22,7 @@ const Contact = () => {
   const handleSubmit = (e) => {};
 
   return (
-    <div className="xl:mt-12 xl:flex-row overflow-hidden">
+    <div className="xl:mt-12 flex xl:flex-row overflow-hidden flex-col-reverse gap-10">
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
         className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
@@ -75,6 +75,13 @@ const Contact = () => {
             {loading ? "Sending..." : "Send"}
           </button>
         </form>
+      </motion.div>
+
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      >
+        <EarthCanvas />
       </motion.div>
     </div>
   );
