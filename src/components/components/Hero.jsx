3bf7626590681bfdacc8e23dff2d0ba90af0bd38
Andrei-Constantin <andrei-constantin.popa@estudiantat.upc.edu>
2024-10-29 21:13:51 +0100

    mobile fix on hero

--- src/components/components/Hero.jsx
+++ src/components/components/Hero.jsx
@@ -7,17 +7,17 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
-        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+        className={`${styles.paddingX} sm:mt-0 mt-7 absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-4 h-4 rounded-full bg-[#5a70f0] mr-7" />
-          <div className="w-1 sm:h-80 h-40 violet-gradient mr-7" />
+          <div className="w-1 sm:h-80 h-60 violet-gradient mr-7" />
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hey! I'm <span className="text-[#5a70f0]">Andrei</span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+          <p className={`${styles.heroSubText} sm:mt-2 mt-4 text-white-100`}>
             I am a Junior Software Engineer & Developer
             <br className="sm:block hidden" /> Welcome to my portofolio!
           </p>
