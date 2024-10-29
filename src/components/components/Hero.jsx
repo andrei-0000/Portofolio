52c937bfe51aa4d61a8404f9ea8c381c486b1ad1
Andrei-Constantin <andrei-constantin.popa@estudiantat.upc.edu>
2024-10-29 20:27:49 +0100

    added AP logo to navbar

--- src/components/components/Hero.jsx
+++ src/components/components/Hero.jsx
@@ -14,11 +14,12 @@ const Hero = () => {
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>Andrei</h1>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hey! I'm <span className="text-[#915EFF]">Andrei</span>
+          </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            i am developer, software, fulstak,{" "}
-            <br className="sm:block hidden" />
-            frontending, the back.
+            I am a Junior Software Engineer & Developer
+            <br className="sm:block hidden" /> Welcome to my portofolio!
           </p>
         </div>
       </div>
--- src/components/components/Navbar.jsx
+++ src/components/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { styles } from "../../styles";
 import { navLinks } from "../../constants";
-import { logo, menu, close } from "../../assets/assets";
+import { logoAndrei, menu, close } from "../../assets/assets";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
@@ -20,7 +20,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <img src={logoAndrei} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
             Andrei Popa &nbsp;
             <span className="sm:block hidden"> | Software Engineer</span>
